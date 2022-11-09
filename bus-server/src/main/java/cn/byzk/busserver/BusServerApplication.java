c13b4aafff02126257f55336fe3c82c1642c05ab
刘凯 <1304697166@qq.com>
2022-11-09 16:13:32 +0800

    上传代码

--- bus-server/src/main/java/cn/byzk/busserver/BusServerApplication.java
+++ bus-server/src/main/java/cn/byzk/busserver/BusServerApplication.java
@@ -2,7 +2,11 @@ package cn.byzk.busserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer  //标记为 Config　Server
+@EnableEurekaClient
 @SpringBootApplication
 public class BusServerApplication {
 
